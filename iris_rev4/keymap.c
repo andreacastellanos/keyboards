986c5ffa1ea2947aa98fe7c2dcb33c7cbf888ca6
andreacastellanos <56420710+wasilewskis@users.noreply.github.com>
2021-01-28 17:37:13 -0800

    move and add keys

--- iris_rev4/keymap.c
+++ iris_rev4/keymap.c
@@ -8,14 +8,12 @@ extern keymap_config_t keymap_config;
 #define _ADJUST 3
 
 enum custom_keycodes {
-	  QWERTY = SAFE_RANGE,
-	  LOWER,
-	  RAISE,
-	  ADJUST,
-	  DEL_PREV,
-	  DEL_NEXT,
-	  NEXT_WORD,
-	  PREV_WORD,
+	QWERTY = SAFE_RANGE,
+	LOWER,
+	RAISE,
+	ADJUST,
+	DEL_PREV,
+	DEL_NEXT,
 };
 
 
@@ -29,37 +27,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
 		 KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
 	  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-		 KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,           KC_DEL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+		 KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LGUI,          KC_MPLY, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
 	  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-										LOWER,   KC_SPC,  KC_LGUI,                   KC_RALT, KC_ENT,  RAISE
+										LOWER,   KC_SPC,  KC_LALT,                   KC_RCTL,  KC_ENT,  RAISE
 									// └────────┴────────┴────────┘                 └────────┴────────┴────────┘
 	),
 
 	[_LOWER] = LAYOUT(
 	  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-		 KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                              KC_NO,   KC_NO, 	KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
+		 KC_PSCR, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_TRNS,
 	  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-		 KC_TRNS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS,
+		 KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_LT,   KC_NO,   					 		  KC_NO,   KC_GT,   KC_MINS, KC_PLUS, KC_EQL,  KC_TRNS,
 	  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-		 KC_LT,   KC_GRV,  KC_SLSH, KC_LBRC, KC_LPRN, KC_LCBR,                            KC_RCBR, KC_RPRN, KC_RBRC, KC_BSLS, KC_UNDS, KC_GT,
+		 KC_TRNS, KC_GRV,  KC_SLSH, KC_LBRC, KC_LPRN, KC_LCBR,                            KC_RCBR, KC_RPRN, KC_RBRC, KC_BSLS, KC_UNDS, KC_TRNS,
 	  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-		 KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_UNDS, KC_TRNS,          KC_NO,   KC_MINS, KC_EQL,  KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
+		 KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS, KC_TRNS,          KC_NO,   KC_NO,  	KC_NO, 	KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
 	  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
 										KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, ADJUST
 									// └────────┴────────┴────────┘                 └────────┴────────┴────────┘
 	),
- 
+
 	[_RAISE] = LAYOUT(
 	  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
 		 KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, 				 			  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
 	  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-		 KC_TRNS, KC_NO,   KC_PGUP, KC_UP,   KC_PGDN, KC_NO, 				 			  KC_MINS, KC_7,    KC_8,    KC_9,    KC_NO,   KC_TRNS, 
+		 KC_TRNS, KC_NO,   KC_PGDN, KC_UP, 	 KC_PGUP, KC_NO, 				 			  KC_MINS, KC_7,    KC_8,    KC_9,    KC_NO,   KC_TRNS, 
 	  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
 		 KC_TRNS, KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, 				 			  KC_EQL,  KC_4,    KC_5,    KC_6,    KC_0,    KC_TRNS, 
 	  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-		 KC_TRNS, KC_PSCR, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS, 		 KC_NO,   KC_PPLS, KC_1,    KC_2,    KC_3,    KC_NO,   KC_TRNS, 
+		 KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,        	 KC_NO,   KC_PPLS, KC_1,    KC_2,    KC_3,    KC_NO,   KC_TRNS, 
 	  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-										KC_TRNS, KC_TRNS, 	ADJUST,  	    		 KC_TRNS, KC_TRNS, KC_TRNS
+										KC_TRNS, KC_TRNS, ADJUST,  	    		     KC_TRNS, KC_TRNS, KC_TRNS
 									// └────────┴────────┴────────┘                 └────────┴────────┴────────┘
 	),
 
@@ -67,11 +65,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
 		 KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6, 			 	 			  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, 
 	  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-		 KC_NO,   KC_NO,   KC_MPRV, KC_MNXT, KC_MPLY, KC_NO, 			 	 			  KC_NO,   KC_VOLU, KC_VOLD, KC_MUTE, KC_NO,   KC_NO, 
+		 KC_NO,   KC_NO,   KC_MPRV, KC_MPLY, KC_MNXT, KC_NO, 			 	 			  KC_NO,   KC_VOLU, KC_VOLD, KC_MUTE, KC_NO,   KC_NO, 
 	  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-		 KC_NO,   KC_NO,   KC_NO,   DEL_PREV,PREV_WORD,KC_NO, 			 	 			  KC_NO,   NEXT_WORD,DEL_NEXT,KC_NO,  KC_NO,   KC_NO, 
+		 KC_NO,   KC_NO,   KC_NO,   KC_NO, 	 DEL_PREV,KC_NO,			 	 			  KC_NO,   DEL_NEXT,KC_NO,   KC_NO,	  KC_NO,   KC_NO, 
 	  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-		 RGB_TOG, RGB_MOD, BL_TOGG, BL_STEP, KC_NO,   KC_NO,   KC_NO,   		 RGB_VAI, RGB_VAD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, KC_NO, 
+		 RGB_TOG, RGB_MOD, BL_TOGG, BL_STEP, KC_NO,   KC_NO,   RESET,   		 KC_NO,   RGB_VAI, RGB_VAD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,
 	  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
 										KC_TRNS, KC_TRNS, KC_TRNS, 		    		 KC_TRNS, KC_TRNS, KC_TRNS
 									// └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -124,16 +122,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 				SEND_STRING(SS_DOWN(X_LCTRL)SS_DOWN(X_LSHIFT)SS_TAP(X_RIGHT)SS_UP(X_LSHIFT)SS_UP(X_LCTRL)SS_TAP(X_DELETE));
 			}
 			break;
-		case NEXT_WORD:
-			if (record->event.pressed) {
-				SEND_STRING(SS_DOWN(X_LCTRL)SS_TAP(X_RIGHT)SS_UP(X_LCTRL));
-			}
-			break;
-		case PREV_WORD:
-			if (record->event.pressed) {
-				SEND_STRING(SS_DOWN(X_LCTRL)SS_TAP(X_LEFT)SS_UP(X_LCTRL));
-			}
-			break;
 		}
 	return true;
 }
@@ -156,30 +144,3 @@ void encoder_update_user(uint8_t index, bool clockwise) {
 		}
 	}
 }
-
-
-// Window Movement
-
-// bool is_alt_tab_active = false;
-// uint16_t alt_tab_timer = 0;
-
-// if (clockwise) {
-//   if (!is_alt_tab_active) {
-//     is_alt_tab_active = true;
-//    register_code(KC_LALT);
-//  }
-//   alt_tab_timer = timer_read();
-//   tap_code16(KC_TAB);
-// } else {
-//   alt_tab_timer = timer_read();
-//   tap_code16(S(KC_TAB));
-// }
-
-// void matrix_scan_user(void) {
-//   if (is_alt_tab_active) {
-//     if (timer_elapsed(alt_tab_timer) > 1250) {
-//       unregister_code(KC_LALT);
-//       is_alt_tab_active = false;
-//     }
-//   }
-// }
