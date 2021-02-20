be7783ca4ef644713e22c62e67dff28aaf222457
andreacastellanos <castellanosp.andrea@gmail.com>
2021-02-20 00:18:19 -0800

    fix visual formatting

--- iris_rev4/keymap.c
+++ iris_rev4/keymap.c
@@ -19,58 +19,58 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_QWERTY] = LAYOUT(
 	  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-		 KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+	     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
 	  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-		 KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+       	     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
 	  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-		 KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+	     KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
 	  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-		 KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LALT,          KC_MPLY, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+	     KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LALT,          KC_MPLY, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
 	  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-										LOWER,   KC_SPC,  KC_LGUI,                   KC_RCTL,  KC_ENT, RAISE
-									// └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+					    LOWER,   KC_SPC,  KC_LGUI,                   KC_RCTL, KC_ENT,  RAISE
+				        // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
 	),
 
 	[_LOWER] = LAYOUT(
 	  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-		 KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6, 			 	 			  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, 
+	     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6, 			      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, 
 	  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-		 KC_TRNS, KC_NO,KC_NO, LALT(KC_LEFT), LALT(KC_RGHT), KC_NO, 				 	  KC_NO,   KC_PGDN, KC_UP, 	 KC_PGUP, KC_NO,   KC_TRNS, 
+	     KC_TRNS, KC_NO,KC_NO, LALT(KC_LEFT), LALT(KC_RGHT), KC_NO, 		      KC_NO,   KC_PGDN, KC_UP, 	 KC_PGUP, KC_NO,   KC_TRNS, 
 	  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-		 KC_TRNS, KC_NO,   KC_BTN3, KC_BTN2, KC_BTN1, KC_WH_U, 				 			  KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_TRNS, 
+	     KC_TRNS, KC_NO,   KC_BTN3, KC_BTN2, KC_BTN1, KC_WH_U, 			      KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_TRNS, 
 	  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-		 KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_WH_D,   KC_NO,        	 KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS, 
+	     KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_WH_D,   KC_NO,          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS, 
 	  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-										KC_TRNS, KC_TRNS, ADJUST,  	    		     KC_TRNS, KC_TRNS, KC_TRNS
-									// └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+				    	    KC_TRNS, KC_TRNS, ADJUST,  	    		 KC_TRNS, KC_TRNS, KC_TRNS
+					// └────────┴────────┴────────┘                 └────────┴────────┴────────┘
 	),
 
 	[_RAISE] = LAYOUT(
 	  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-		 KC_PSCR, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_TRNS,
+	     KC_PSCR, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS,
 	  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-		 KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_LT,   					 		  KC_GT,   KC_MINS, KC_PLUS, KC_EQL,  KC_NO,   KC_TRNS,
+	     KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_LT,   			      KC_GT,   KC_MINS, KC_PLUS, KC_EQL,  KC_NO,   KC_TRNS,
 	  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-		 KC_TRNS, KC_GRV,  KC_SLSH, KC_LBRC, KC_LPRN, KC_LCBR,                            KC_RCBR, KC_RPRN, KC_RBRC, KC_BSLS, KC_UNDS, KC_TRNS,
+	     KC_TRNS, KC_GRV,  KC_SLSH, KC_LBRC, KC_LPRN, KC_LCBR,                            KC_RCBR, KC_RPRN, KC_RBRC, KC_BSLS, KC_UNDS, KC_TRNS,
 	  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-		 KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS, KC_TRNS,          KC_NO,   KC_NO,  	KC_NO, 	KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
+	     KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS, KC_TRNS,          KC_NO,   KC_NO,  	KC_NO, 	KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
 	  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-										KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, ADJUST
-									// └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+					    KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, ADJUST
+					// └────────┴────────┴────────┘                 └────────┴────────┴────────┘
 	),
 
 	[_ADJUST] = LAYOUT(
 	  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-		 KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, 				 			  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
+	     KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, 			      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
 	  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-		 KC_NO,   KC_NO,   KC_MPRV, KC_MPLY, KC_MNXT, KC_NO, 			 	 			  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, 
+	     KC_NO,   KC_NO,   KC_MPRV, KC_MPLY, KC_MNXT, KC_NO, 			      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, 
 	  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-		 KC_NO,   KC_NO,   KC_NO,   KC_NO, 	 KC_NO,   KC_NO,			 	 			  KC_NO,   KC_NO,   KC_NO,   KC_NO,	  KC_NO,   KC_NO,
+	     KC_NO,   KC_NO,   KC_NO,   KC_NO, 	 KC_NO,   KC_NO,			      KC_NO,   KC_NO,   KC_NO,   KC_NO,	  KC_NO,   KC_NO,
 	  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-		 RGB_TOG, RGB_MOD, BL_TOGG, BL_STEP, KC_NO,   KC_NO,   RESET,   		 KC_NO,   RGB_VAI, RGB_VAD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,
+	     RGB_TOG, RGB_MOD, BL_TOGG, BL_STEP, KC_NO,   KC_NO,   RESET,   	     KC_NO,   RGB_VAI, RGB_VAD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,
 	  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-										KC_TRNS, KC_TRNS, KC_TRNS, 		    		 KC_TRNS, KC_TRNS, KC_TRNS
-									// └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+					    KC_TRNS, KC_TRNS, KC_TRNS, 		         KC_TRNS, KC_TRNS, KC_TRNS
+					// └────────┴────────┴────────┘                 └────────┴────────┴────────┘
 	),
 };
 
